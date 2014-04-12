255c6df2d34e5338cea0d208c1c380bf58664b82
Jordan Snelling <jordach.snelling@gmail.com>
2014-04-12 12:24:19 +0100

    add LICENSE, notes, license all the things.
    updated a few skins, make them more better. (and remove shittlily done
    capes that i made by accident.)

--- big_funking_dig/mods/ores/oregen.lua
+++ big_funking_dig/mods/ores/oregen.lua
@@ -1,3 +1,19 @@
 --oregen.lua
---ores mod for BFD.
 
+ -- This file is part of BFD.
+
+    -- BFD is free software: you can redistribute it and/or modify
+    -- it under the terms of the GNU General Public License as published by
+    -- the Free Software Foundation, either version 3 of the License, or
+    -- (at your option) any later version.
+
+    -- BFD is distributed in the hope that it will be useful,
+    -- but WITHOUT ANY WARRANTY; without even the implied warranty of
+    -- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    -- GNU General Public License for more details.
+	
+	-- You may contact Jordach via the Minetest Forums PM service for help
+	-- or ask on the forum topic for which this game is set on.
+
+    -- You should have received a copy of the GNU General Public License
+    -- along with BFD.  If not, see <http://www.gnu.org/licenses/>.
