4160406d62684e4a1057cd067187fe1bd2e51ca6
Jordach <jordach.snelling@gmail.com>
2014-12-26 18:57:57 +0000

    fix trash cans deleting kittens :(

--- mods/trash_can/init.lua
+++ mods/trash_can/init.lua
@@ -77,13 +77,13 @@ minetest.register_craft({
 --Throw In Trash Can Code
 
 local old_on_step = minetest.registered_entities["__builtin:item"].on_step
-minetest.registered_entities["__builtin:item"].on_step = function(self, dtime)
-    if minetest.env:get_node(self.object:getpos()).name == "trash_can:trash_can_wooden" then
-        self.object:remove()
-        return
-    end
-    old_on_step(self, dtime)
-end
+-- minetest.registered_entities["__builtin:item"].on_step = function(self, dtime)
+    -- if minetest.env:get_node(self.object:getpos()).name == "trash_can:trash_can_wooden" then
+        -- self.object:remove()
+        -- return
+    -- end
+    -- old_on_step(self, dtime)
+-- end
 
 --Unused stuff
 --minetest.register_node("trash_can:trash_can_full",{
